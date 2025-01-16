91422f0f280c090ec3fee6a94a112bbe3c2b52ac
Botir <tiboruz@gmail.com>
2025-01-16 14:38:28 +0500

    3rd commit

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PosController extends Controller
 {
@@ -61,7 +62,51 @@ class PosController extends Controller
     {
         //
     }
-     public function load(){
+    public function load(){
+       
+       
+        if (!Storage::disk('local')->exists('test/zag.spr')) {
+            $data=Product::withCount('prices')->get();
+            $content=$this->text();
+            foreach($data as $item){
+               // 669;;ARTEL SI 2531;ARTEL SI 2531;430000;0;;0,1,1,1,1,1,1,1,1,0,1,0,0,1;;;;1;1;;;1;1;;;;;;4;;;ARTEL0030;;;;;;;;;;;;;;;;;;;;;;;;;;;;;0;0;;2;;
+                $content=$content.$item->id.";;".$item->name.";".$item->name.";".$item->prices[$item->prices_count-1]->price.";0;;0,1,1,1,1,1,1,1,1,0,1,0,0,1;;;;1;1;;;1;1;;;;;;4;;;".$item->sap.";;;;;;;;;;;;;;;;;;;;;;;;;;;;;0;0;;2;;\n";
+    
+            }
+            
+            $file=Storage::disk('local')->put('test/zag.txt',$content);
+            Storage::disk('local')->put('test/zag.spr','');
+            return to_route('shop.index');
+        }else{
+            //dd('error');
+            return to_route('shop.index')->withErrors('there file on porcess try later');
+        }
+        
+        //dd($file);
 
-     }
+    }
+    protected function text(){
+        $content="##@@&&\n#\n$$\$ADDASPECTSCHMS\n1;Характеристики;Характеристики;\n$$\$ADDASPECTS/n1;1;Характеристики;Характеристики;\n$$\$ADDTAXRATES";
+        $content=$content."1;НДС 0%;НДС 0%;0;0;\n";
+        $content=$content."2;НДС 10%;НДС 10%;0;10;\n";
+        $content=$content."3;НДС 18%;НДС 18%;0;18;\n";
+        $content=$content."4;Без НДС;Без НДС;0;0;\n";
+        $content=$content."$$\$ADDTAXGROUPS\n";
+        $content=$content."1;НДС 0%;НДС 0%;\n";
+        $content=$content."2;НДС 10%;НДС 10%;\n";
+        $content=$content."3;НДС 18%;НДС 18%;\n";
+        $content=$content."4;Без НДС;Без НДС;\n";
+        $content=$content."$$\$ADDTAXGROUPRATES\n";
+        $content=$content."1;1;1;0;\n";
+        $content=$content."1;2;2;0;\n";
+        $content=$content."1;3;3;0;\n";
+        $content=$content."1;4;4;0;\n\n";
+        
+        $content=$content."$$\$ADDASPECTVALUES\n\n";
+        
+        $content=$content."$$\$ADDQUANTITY\n";
+        
+        
+        return $content;
+    }
 }
--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -5,7 +5,7 @@
     <div class="nk-block-head nk-block-head-sm">
         <div class="nk-block-between g-3">
             <div class="nk-block-head-content">
-                <h3 class="nk-block-title page-title">POS ga yuklash</h3>
+                <h3 class="nk-block-title page-title">POS Lar</h3>
                 <div class="nk-block-des text-soft">
                     
                 </div>
@@ -16,74 +16,7 @@
         </div><!-- .nk-block-between -->
     </div><!-- .nk-block-head -->
     <div class="nk-block">
-        <div class="card card-bordered card-stretch">
-            <div class="card-inner-group">
-
-                <div class="card-inner p-0">
-                    <table class="table table-tranx">
-                        <thead>
-                            <tr class="tb-tnx-head">
-                                <th class="tb-tnx-id"><span class="">SAP</span></th>
-                                <th class="tb-tnx-info">
-                                    Gruppa
-                                </th>
-                                <th class="tb-tnx-info">
-                                    <span class="tb-tnx-total">Maxsulot</span>
-                                   
-                                </th>
-                                <th class="tb-tnx-info">
-                                    Narx
-                                   
-                                </th>
-                                
-                            </tr><!-- tb-tnx-item -->
-                        </thead>
-                        <tbody>
-                            @foreach($data as $item)
-                            <tr class="tb-tnx-item">
-                                <td class="tb-tnx-id">
-                                    <a href="{{route('product.show',['product'=>$item->id])}}"><span>{{$item->sap}}</span></a>
-                                </td>
-                                <td class="tb-tnx-info">
-                                    
-                                        <span class="title">{{$item->group}}</span>
-                              
-                                  
-                                </td>
-                                <td class="tb-tnx-amount is-alt">
-                                   
-                                        <span class="amount">{{$item->name}}</span>
-                                    
-                                    
-                                </td>
-                                <td class="tb-tnx-amount is-alt">
-                                
-                                        <span class="amount">{{$item->prices[$item->prices_count-1]->price}}</span>
-                                   
-                                    
-                                </td>
-                                
-                            </tr><!-- tb-tnx-item -->
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div><!-- .card-inner -->
-
-                <div class="card-inner">
-                {{$data->links()}}    
-                
-                {{--<ul class="pagination justify-content-center justify-content-md-start">
-                        <li class="page-item"><a class="page-link" href="#">Prev</a></li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><span class="page-link"><em class="icon ni ni-more-h"></em></span></li>
-                        <li class="page-item"><a class="page-link" href="#">6</a></li>
-                        <li class="page-item"><a class="page-link" href="#">7</a></li>
-                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                    </ul>--}}
-                </div><!-- .card-inner -->
-            </div><!-- .card-inner-group -->
-        </div><!-- .card -->
+        
     </div><!-- .nk-block -->
 </div>
 
